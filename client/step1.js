f8db8789e869dd9dbf3c57790d49ff34aa51e1e9
Larisa Berger <larisa@org.org>
2012-10-05 18:29:30 -0400

    updated step1.js to clear old values in table when updating.

--- client/step1.js
+++ client/step1.js
@@ -1,41 +1,50 @@
+var summaryEmpty = true; //flag for enrollment summary state
+
 Template.Step_1.events({
     'click #calculateStep1':function() {    
-
-	console.log("in function calculateStep1");
-	
-
-	//Total Patient Enrollment Row
-	var initial = parseInt($('#initialEnroll').val());
-	var target = parseInt($('#year5Enroll').val());
-	
-	var newHtml = '<td>' + initial + '</td>';
-	var currCell = $('#enrollSummary tr:eq(1) td:eq(0)');
-	for (year=1; year<6; year+=1){
-            $(newHtml).insertAfter(currCell);
-            var numPatients = initial + (((target-initial)/4)*year);
-            newHtml = '<td>' + numPatients + '</td>'
-            currCell = $('#enrollSummary tr:eq(1) td:eq(' + year +')');
-	}
-	
-	//Concurrent Patient Enrollment Row
-
-	var totalMonths = parseInt($('#monthsEnroll').val());
-	var currPatientTot = $('#enrollSummary tr:eq(1) td:eq(1)').text();
-	var currCell = $('#enrollSummary tr:eq(2) td:eq(0)');
-
-	for (year=1; year<6; year+=1){
-            var numPatients = (currPatientTot*totalMonths)/12;
-            var newHtml = '<td>' + numPatients + '</td>';
-            $(newHtml).insertAfter(currCell);
-            
-	    currCell = $('#enrollSummary tr:eq(2) td:eq(' + year + ')');
-            currPatientTotObject = $('#enrollSummary tr:eq(1) td:eq(' + (year+1) + ')');
-            currPatientTot = currPatientTotObject.text();
-	};
-	
-	
-	
+        updateSummary();
     }
-})
+});
+
+
+function updateSummary(){
+    //Total Patient Enrollment Row
+    var initial = parseInt($('#initialEnroll').val());
+    var target = parseInt($('#year5Enroll').val());
+    
+    var newHtml = '<td>' + initial + '</td>';
+    currCell = $('#enrollSummary tr:eq(1) td:eq(1)');
+    currCell.html(Math.floor(initial));
+    for (year=2; year<6; year+=1){
+//        $(newHtml).insertAfter(currCell);
+        currCell = $('#enrollSummary tr:eq(1) td:eq(' + year +')');
+        console.log('curr cell html');
+        console.log($(currCell.html()));
+        var numPatients = initial + (((target-initial)/4)*year);
+        currCell.html(Math.floor(numPatients));
+  //      newHtml = '<td>' + numPatients + '</td>'
+
+    }
+    
+    //Concurrent Patient Enrollment Row
+
+    var totalMonths = parseInt($('#monthsEnroll').val());
+    var currPatientTot = $('#enrollSummary tr:eq(1) td:eq(1)').text();
+
+    for (year=1; year<6; year+=1){
+	currCell = $('#enrollSummary tr:eq(2) td:eq(' + year + ')');
+        var numPatients = (currPatientTot*totalMonths)/12;
+        currCell.html(Math.floor(numPatients));
+        currPatientTotObject = $('#enrollSummary tr:eq(1) td:eq(' + (year+1) + ')');
+        currPatientTot = currPatientTotObject.text();
+    }
+
+
+}
+
+
+
+
+
 
 
